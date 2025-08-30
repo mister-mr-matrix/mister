a7962bd8c2da2b9be2877c6b4d9aad3d22d9c1c6
quantumvividly@users <229402092+quantumvividly@users.noreply.github.com>
2025-08-30 11:29:49 +0200

    logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import '../app.css';
-	// import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from 'svelte-sonner';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<!-- <link rel="icon" href={favicon} /> -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+	<title>Mister - Matrix Registration</title>
 </svelte:head>
 
 <Toaster />
