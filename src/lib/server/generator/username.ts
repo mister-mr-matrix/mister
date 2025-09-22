f91465a1e6142662c0d812d4703e5ba35c076403
quantumvividly@users <229402092+quantumvividly@users.noreply.github.com>
2025-09-22 02:06:45 +0200

    opt username api

--- src/lib/server/generator/username.ts
+++ src/lib/server/generator/username.ts
@@ -1,11 +1,17 @@
+import { env } from '$env/dynamic/private';
 import generator from 'eff-diceware-passphrase';
 import { availableUsername } from '$lib/server/matrix/available';
+
+type UsernameAPIResponse = {
+	username: string;
+};
 
 export async function generateUsername(skip?: boolean): Promise<string> {
 	const tries = 5;
 
 	for (let i = 0; i < tries; i++) {
-		const username = generator(2).join('');
+		const username =
+			env.MR_USERNAME_API_URL === undefined ? generator(2).join('') : await getUsernameFromAPI();
 		if (skip) {
 			return username;
 		}
@@ -17,4 +23,35 @@ export async function generateUsername(skip?: boolean): Promise<string> {
 	}
 
 	throw new Error(`Failed to generate an available username ${tries} times in a row`);
+}
+
+async function getUsernameFromAPI(): Promise<string> {
+	if (env.MR_USERNAME_API_URL === undefined) {
+		throw new Error('MR_USERNAME_API_URL env var not set');
+	}
+
+	const url = new URL(env.MR_USERNAME_API_URL);
+
+	try {
+		const response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const result: UsernameAPIResponse = await response.json();
+
+		if (!response.ok) {
+			throw new Error(`Username availability check failed: ${JSON.stringify(result)}`);
+		}
+
+		if (result.username) {
+			console.debug('Username found in the API response:', JSON.stringify(result));
+			return result.username;
+		} else {
+			throw new Error(`No username found in the API response: ${JSON.stringify(result)}`);
+		}
+	} catch (error) {
+		throw new Error(`Error getting the username from the API: ${error}`);
+	}
 }
